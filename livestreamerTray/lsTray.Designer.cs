f4ccac0d0d6f6e56af8e9cb5ca30a1e88a146436
warcode <warcode@deny.io>
2015-04-01 17:07:54 +0200

    Init

---
+++ livestreamerTray/lsTray.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace livestreamerTray
+{
+    partial class lsTray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lsTray));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "lsTray";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // lsTray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "lsTray";
+            this.Text = "livestreamerTray";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.lsTray_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ livestreamerTray/lsTray.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace livestreamerTray
+{
+    public partial class lsTray : Form
+    {
+        public lsTray()
+        {
+            InitializeComponent();
+
+            this.ShowInTaskbar = false;
+            this.Visible = false;
+        }
+
+        private void lsTray_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string url;
+
+            if(!System.Windows.Forms.Clipboard.ContainsText())
+            {
+                notifyIcon1.ShowBalloonTip(250, "No Link Provided", "Copy a twitch.tv stream link before clicking here.", ToolTipIcon.None);
+                return;
+            }
+            else
+            {
+                url = System.Windows.Forms.Clipboard.GetText(TextDataFormat.Text);
+            }
+
+            Regex linkParser = new Regex(@"\b(?:https?://|www\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var matches = linkParser.Matches(url);
+
+            if(matches.Count == 0)
+            {
+                notifyIcon1.ShowBalloonTip(250, "No Link Provided", "Copy a twitch.tv stream link before clicking here.", ToolTipIcon.None);
+                return;
+            }
+            else
+            {
+                url = matches[0].Value;
+            }
+
+            url = url.Replace("http://www.", "");
+            url = url.Replace("http://", "");
+
+            var lsProcessContainer = new Process();
+            lsProcessContainer.StartInfo.FileName = "cmd.exe";
+
+            var applicationDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var livestreamerLocation = applicationDirectory + @"\livestreamer\";
+
+            lsProcessContainer.StartInfo.Arguments = string.Format(@"/c {0}livestreamer.exe --config {1}\livestreamer.cfg {2}", livestreamerLocation, applicationDirectory, url);
+
+            lsProcessContainer.StartInfo.RedirectStandardOutput = true; //true
+            lsProcessContainer.StartInfo.CreateNoWindow = true; // true
+            lsProcessContainer.StartInfo.UseShellExecute = false; // false
+            try
+            {
+
+                notifyIcon1.ShowBalloonTip(100, "Opening stream", url, ToolTipIcon.None);
+
+                lsProcessContainer.Start();
+
+                if(lsProcessContainer.HasExited && lsProcessContainer.ExitCode == 1)
+                {
+                    notifyIcon1.ShowBalloonTip(250, "Error", "Could not upen that url", ToolTipIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                notifyIcon1.ShowBalloonTip(250, "Error", ex.Message, ToolTipIcon.Error);
+                this.Close();
+            }
+        }
+    }
+}
